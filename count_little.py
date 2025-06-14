6172ee1fa477be8aed4d68febfae798e21bc5d77
unknown <igorbrandt95@hotmail.com>
2025-06-14 16:24:46 -0300

    :sparkles: resolvi o exercicio

---
+++ count_little.py
@@ -0,0 +1,20 @@
+sentenca = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go"
+
+def contador_de_littles(sentenca):
+    # separar string em lista de palavras
+    palavras = []
+    sentenca_strip = sentenca.lower().split()
+    for palavra in sentenca_strip:
+        palavras.append(palavra)
+    print(palavras)
+    
+    # conta quantos littles
+    contagem_littles = 0
+    for palavra in palavras:
+        if palavra == "little":
+            contagem_littles += 1
+    
+    print(f"A palavra little aparece {contagem_littles} vezes na sentença")
+
+
+contador_de_littles(sentenca)
