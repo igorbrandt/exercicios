3f3f341ee01d044a83437c80351ff9ee81945548
unknown <igorbrandt95@hotmail.com>
2025-06-14 16:35:49 -0300

    :wrench: atualizei func para ser um contador de palavras, e não apenas de little

--- count_little.py
+++ count_little.py
@@ -1,20 +1,21 @@
 sentenca = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go"
 
-def contador_de_littles(sentenca):
+PALAVRA_PROCURADA = "little"
+
+def contador_de_palavras(sentenca):
     # separar string em lista de palavras
     palavras = []
     sentenca_split = sentenca.lower().split()
-    for palavra in sentenca_strip:
+    for palavra in sentenca_split:
         palavras.append(palavra)
     print(palavras)
     
     # conta quantos littles
-    contagem_littles = 0
+    contagem_palavras = 0
     for palavra in palavras:
-        if palavra == "little":
-            contagem_littles += 1
+        if palavra == PALAVRA_PROCURADA:
+            contagem_palavras += 1
     
-    print(f"A palavra little aparece {contagem_littles} vezes na sentença")
+    print(f"A palavra little aparece {contagem_palavras} vezes na sentença")
 
-
-contador_de_littles(sentenca)
+contador_de_palavras(sentenca)
