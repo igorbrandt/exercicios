4429e5a5190695f30b9941da4d0b7d9c793dac1a
unknown <igorbrandt95@hotmail.com>
2025-06-14 16:41:24 -0300

    :wrench: corrigi texto no print da funcao

--- count_little.py
+++ count_little.py
@@ -8,7 +8,6 @@ def contador_de_palavras(sentenca):
     sentenca_split = sentenca.lower().split()
     for palavra in sentenca_split:
         palavras.append(palavra)
-    print(palavras)
     
     # conta quantos littles
     contagem_palavras = 0
@@ -16,6 +15,7 @@ def contador_de_palavras(sentenca):
         if palavra == PALAVRA_PROCURADA:
             contagem_palavras += 1
     
-    print(f"A palavra little aparece {contagem_palavras} vezes na sentença")
+    print(f'''
+          A palavra procurada *{PALAVRA_PROCURADA}* aparece {contagem_palavras} vezes na sentença\n''')
 
 contador_de_palavras(sentenca)
